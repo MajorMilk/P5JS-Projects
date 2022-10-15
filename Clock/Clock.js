8cf96f75cccbeace9fa4edaec1a998d0ce4096bc
MajorMilk <captainjay111@gmail.com>
2022-10-15 02:52:12 -0400

    update

--- Clock/Clock.js
+++ Clock/Clock.js
@@ -5,6 +5,8 @@ function setup() {
   createCanvas(400,400);
   FRAMECOUNT = 0;
   MONTHLENGTHA = [31,29,31,30,31,30,31,31,30,31,30,31];
+  
+  Sec = second();
 }
 //At 0 then top of circle
 //200,350
@@ -16,13 +18,12 @@ function setup() {
 function draw() {
   background(0);
   if(FRAMECOUNT % 2 === 0) {
-    Millis = millis();
-    SecondHandA = map(Sec, 0 , 60, 0, 360);
+    SecondHandA = map(Sec, 0 , 60, 0, TWO_PI);
     if(FRAMECOUNT % 10 === 0) {
       Sec = second();
       Minute = minute();
       
-      MinuteHandA = map(Minute, 0 , 60, 0, 360);
+      MinuteHandA = map(Minute, 0 , 60, 0, TWO_PI);
       if(FRAMECOUNT % 100 === 0) {
         FRAMECOUNT = 0;
         Year = year();
@@ -35,7 +36,7 @@ function draw() {
         MonthA = map(Day, 0, DAYSINMONTH, 0, TWO_PI);
         DayA = map(Hour, 0 ,24, 0, TWO_PI);
         HourA = map(Minute%60, 0 , 60, 0, TWO_PI);
-        HourHandA = map(Hour % 12, 0, 12, 0, 360);
+        HourHandA = map(Hour % 12, 0, 12, 0, TWO_PI);
       }
     }
     
@@ -53,7 +54,7 @@ function draw() {
   //Day w = 300
   angleMode(RADIANS)
   noFill();
-  strokeWeight(4);
+  strokeWeight(3);
   stroke(3,255,194);
   arc(200, 200, 350, 350, 0, YearA);
   stroke(255,3,68);
@@ -64,25 +65,25 @@ function draw() {
   translate(200,200);
   
   
-  angleMode(DEGREES);
+  //angleMode(DEGREES);
   
-  rotate(180);
-  rectMode(CENTER);
-  rotate(HourHandA);
+  rotate(PI);
+  //rectMode(CENTER);
+  //rotate(HourHandA);
   //line(0, 0, 0, 50);
-  triangle(-50,0 ,50,0, 0,50);
-  rotate(360-HourHandA);
+  triangle(-50,0 ,50,0, (150 * sin(-HourHandA)), (75 * cos(-HourHandA)));
+  //rotate(360-HourHandA);
   
   stroke(3,206,255);
-  rotate(MinuteHandA);
+  //rotate(MinuteHandA);
   //line(0, 0, 0, 75);
-  triangle(-75,0 ,75,0, 0,75);
-  rotate(360-MinuteHandA);
+  triangle(-50,0 ,50,0, (150 * sin(-MinuteHandA)), (100 * cos(-MinuteHandA)));
+  //rotate(360-MinuteHandA);
   
-  stroke(141,13,255);
-  rotate(SecondHandA);
+  stroke(255);
+  //rotate(SecondHandA);
   //line(0, 0, 0, 75);
-  triangle(-75,0 ,75,0, 0,75);
+  triangle(-50,0 ,50,0, (150 * sin(-SecondHandA)), (100 * cos(-SecondHandA)));
   rotate(360-SecondHandA);
   rotate(180);
   translate(-200,-200);
